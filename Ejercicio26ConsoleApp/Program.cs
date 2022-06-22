a00e7cd690814df4574e5586f861a536a331ca80
ciminoce <ciminoce@yahoo.com.ar>
2022-06-22 09:54:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio26ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio26ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vocales = "aAeEiIoOuU";
+                          
+            Console.WriteLine("Vocales");
+            Console.Write("Ingrese una letra:");
+            char letra = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if (vocales.IndexOf(letra)>=0)
+            {
+                System.Console.WriteLine($"{letra} es una vocal y está en la posición {vocales.IndexOf(letra)}");
+            }
+            else
+            {
+                System.Console.WriteLine($"{letra} no es una vocal");
+            }
+
+            Console.ReadLine();
+            //if (letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u' )
+            //{
+            //    Console.WriteLine($"{letra} es una vocal");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{letra} no es una vocal");
+            //}
+        }
+    }
+}
